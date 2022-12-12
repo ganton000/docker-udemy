aa39147fd7bbe379a4ecab6658061cd9901f8c8b
ganton000 <SendittoGeorgeAnton@gmail.com>
2022-12-12 17:50:06 -0500

    Created anon volume for temp directory

--- docker-node-volumes-example/server.js
+++ docker-node-volumes-example/server.js
@@ -32,6 +32,8 @@ app.post("/create", async (req, res) => {
     const tempFilePath = path.join(__dirname, "temp", adjTitle + ".txt");
     const finalFilePath = path.join(__dirname, "feedback", adjTitle + ".txt");
 
+    console.log("Test Log!");
+
     await fs.writeFile(tempFilePath, content);
 
     access(finalFilePath, fs.constants.F_OK, async (err) => {
